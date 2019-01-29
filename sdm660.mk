491210f6d27aec037df33b55ae30152f36c17951
dianlujitao <dianlujitao@lineageos.org>
2019-01-29 13:39:57 +0800

    sdm660-common: Build camera HAL from source
    Change-Id: I54e122b3df6308e205d8ee93d55a3adf7469412d
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -141,6 +141,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.6 \
+    camera.sdm660 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0.vendor
 
