1b4d027bb4221832e5221e289618a0da0492b89d
Bruno Martins <bgcngm@gmail.com>
2020-11-24 11:19:20 +0000

    sdm660: camera: Switch to display_headers lib include
    LOCAL_COPY_HEADERS is deprecated and Qualcomm HALs have been
    updated to remove its usage. As a consequence, the display headers
    are no longer copied to TARGET_OUT_HEADERS, so use the equivalent
    header lib instead.
    
    Change-Id: I85d229e536146b86662e9c9a28017d2448c856dd
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- QCamera2/Android.mk
+++ QCamera2/Android.mk
@@ -147,6 +147,7 @@ LOCAL_HEADER_LIBRARIES += libandroid_sensor_headers
 LOCAL_HEADER_LIBRARIES += libcutils_headers
 LOCAL_HEADER_LIBRARIES += libsystem_headers
 LOCAL_HEADER_LIBRARIES += libhardware_headers
+LOCAL_HEADER_LIBRARIES += display_headers
 
 #HAL 1.0 Include paths
 LOCAL_C_INCLUDES += \
@@ -175,8 +176,6 @@ endif
 LOCAL_CFLAGS += -DUSE_CAMERA_METABUFFER_UTILS
 
 #LOCAL_STATIC_LIBRARIES := libqcamera2_util
-LOCAL_C_INCLUDES += \
-        $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
         hardware/qcom/display/libqservice
 LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync
