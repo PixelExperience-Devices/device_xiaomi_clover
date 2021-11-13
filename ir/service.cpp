LineageOS/android_device_xiaomi_sdm660-common

sdm660-common: ir: Binderize
Change-Id: Ib704750a56eec2e26440bdfee92976776db58812

ir/service.cpp
@@ -8,10 +8,30 @@
 
 #include <android/hardware/ir/1.0/IConsumerIr.h>
 #include <hidl/LegacySupport.h>
+#include "ConsumerIr.h"
 
 using android::hardware::ir::V1_0::IConsumerIr;
-using android::hardware::defaultPassthroughServiceImplementation;
+using android::hardware::ir::V1_0::implementation::ConsumerIr;
+using android::hardware::configureRpcThreadpool;
+using android::hardware::joinRpcThreadpool;
 
 int main() {
-    return defaultPassthroughServiceImplementation<IConsumerIr>();
+    android::sp<ConsumerIr> service = new ConsumerIr();
+
+    if (!service->isOk()) {
+        ALOGE("Instance of ConsumerIr failed to init");
+        return 1;
+    }
+
+    configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    android::status_t status = service->registerAsService();
+    if (status != android::OK) {
+        ALOGE("Cannot register ConsumerIr service");
+        return 1;
+    }
+
+    joinRpcThreadpool();
+
+    return 0; // should never get here
 }

PixelExperience-Devices/device_xiaomi_clover

Signed-off-by: clarencelol <clarencekuiek@icloud.com>
Signed-off-by: OdSazib <odsazib@gmail.com>

ir/service.cpp
@@ -6,12 +6,27 @@
 
 #define LOG_TAG "android.hardware.ir@1.0-service"
 
-#include <android/hardware/ir/1.0/IConsumerIr.h>
-#include <hidl/LegacySupport.h>
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+#include "ConsumerIr.h"
 
 using android::hardware::ir::V1_0::IConsumerIr;
-using android::hardware::defaultPassthroughServiceImplementation;
+using android::hardware::ir::V1_0::implementation::ConsumerIr;
+using android::hardware::configureRpcThreadpool;
+using android::hardware::joinRpcThreadpool;
 
 int main() {
-    return defaultPassthroughServiceImplementation<IConsumerIr>();
+    android::sp<IConsumerIr> service = new ConsumerIr();
+
+    configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    android::status_t status = service->registerAsService();
+    if (status != android::OK) {
+        LOG(ERROR) << "Cannot register ConsumerIr service";
+        return 1;
+    }
+
+    joinRpcThreadpool();
+
+    return 1; // should never get here
 }