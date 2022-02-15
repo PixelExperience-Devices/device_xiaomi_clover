4421413792b119b30f095d4f347f3a5b39d1d0f5
Chenyang Zhong <zhongcy95@gmail.com>
2022-02-15 11:46:54 -0500

    clover: switch to retrofit dynamic partitioning

--- device.mk
+++ device.mk
@@ -364,6 +364,10 @@ PRODUCT_PACKAGES += \
     CloverFrameworksOverlay \
     CloverSystemUIOverlay
     
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
