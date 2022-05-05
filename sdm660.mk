59477b8f9cf3299408768644a87f354b3a0d4d54
pix106 <sbordenave@gmail.com>
2022-05-05 14:59:13 +0200

    sdm660-common: move folio_daemon build to device tree [1/2]
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -202,10 +202,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
     android.hardware.drm@1.4.vendor
 
-# Folio
-PRODUCT_PACKAGES += \
-    folio_daemon
-
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
