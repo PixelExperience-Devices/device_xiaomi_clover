9f2276b5e9c0e0db7176fff10c74b90cb34696c0
Alex Hong <rurumihong@google.com>
2022-09-21 20:43:00 +0800

    sdm660-common: Replace the HIDL with AIDL for android.hardware.memtrack  android.hardware.memtrack The HIDL interface of android.hardware.memtrack is  no longer supported for Android S.
    Test: atest VtsHalMemtrackTargetTest pass
    Bug: 186539435
    Change-Id: Iaafd526fc5a601f7e8189ec764399d99c660beb0
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -183,8 +183,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
+    android.hardware.memtrack-service.example \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     gralloc.sdm660 \
