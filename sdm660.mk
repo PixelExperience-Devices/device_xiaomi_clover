2c6f8ea3d775379cae737ed09ace4e13091d8252
pix106 <sbordenave@gmail.com>
2022-10-01 07:41:04 +0200

    sdm660-common: remove duplicate tinyxml

--- sdm660.mk
+++ sdm660.mk
@@ -191,7 +191,6 @@ PRODUCT_PACKAGES += \
     hwcomposer.sdm660 \
     memtrack.sdm660 \
     libdisplayconfig \
-    libtinyxml \
     vendor.display.config@1.0 \
     vendor.display.config@1.0.vendor \
     vendor.display.config@2.0 \
