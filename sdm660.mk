c8d924ba0aaa6c7aa741d41f7fd184f0647619d8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-10-06 15:41:51 +0530

    sdm660-common: Move to common Xiaomi lights AIDL
    Change-Id: Icb3bb31ebad01519b91a6d41b47e1b57e1ac84e1
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -329,7 +329,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.lights-service.xiaomi_sdm660
+    android.hardware.light-service.xiaomi
 
 # Media
 PRODUCT_PACKAGES += \
