ElectroPerf/android_device_asus_sdm660-common

sdm660-common: configs: Import keylayout from LA.UM.10.2.1.r1-03400-sdm660.0
Change-Id: Ibb9cb3d1b5670d795d5e8ab050b7a967e1794c66

sdm660.mk
@@ -11,6 +11,10 @@ PRODUCT_PACKAGES += \
 # Board Platform
 TARGET_BOARD_PLATFORM := sdm660
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/keylayout/,$(TARGET_COPY_OUT_VENDOR)/usr/keylayout)
+
 # IPC router config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config

PixelExperience-Devices/device_xiaomi_clover

Signed-off-by: pix106 <sbordenave@gmail.com>

sdm660.mk
@@ -321,8 +321,7 @@ PRODUCT_COPY_FILES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
-    $(COMMON_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
+    $(call find-copy-subdir-files,*,$(COMMON_PATH)/configs/keylayout/,$(TARGET_COPY_OUT_VENDOR)/usr/keylayout)
 
 # Keystore
 PRODUCT_PACKAGES += \