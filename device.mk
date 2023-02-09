edd988197b2a0113bedfe6efe589cf1acc788807
Alcatraz323 <alcatraz32323@gmail.com>
2023-02-09 14:31:05 +0800

    clover: disable hardware soundtrigger interface
    * ok google doesn't support hardware interface for sdm660, let ok google use software implementation

--- device.mk
+++ device.mk
@@ -39,7 +39,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl:32 \
     android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.3-impl \
     audio.primary.sdm660 \
     audio.r_submix.default \
     audio.usb.default \
