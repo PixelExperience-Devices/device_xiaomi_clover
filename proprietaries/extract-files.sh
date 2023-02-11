355b3d488af85d3a159ff39de7bb42afd932e6cd
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-02-11 22:50:13 +0800

    clover: stage intree IFAAService
    * from hardware_xiaomi

--- proprietaries/extract-files.sh
+++ proprietaries/extract-files.sh
@@ -72,6 +72,15 @@ function blob_fixup() {
         "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
         ;;
 
+    vendor/bin/mlipayd@1.1)
+           "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
+            ;;
+        
+    vendor/lib64/libmlipay.so | vendor/lib64/libmlipay@1.1.so)
+            "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
+            sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" "${2}"
+            ;;
+
     esac
 }
 
