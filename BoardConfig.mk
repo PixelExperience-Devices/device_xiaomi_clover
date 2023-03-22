85697c232accafaabd33ce588f6f513b37777051
Alcatraz323 <alcatraz32323@gmail.com>
2023-03-22 18:24:27 +0800

    clover: remove duplicated prop definition
    * passthrough already defined in system.prop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -152,7 +152,6 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/configs/properties/odm.prop
-TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/properties/product.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/properties/system_ext.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/properties/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
