023193a1004d19d13683755f223ee4baebde0520
Alcatraz323 <alcatraz32323@gmail.com>
2023-03-27 22:34:59 +0800

    clover: use QTI USB 1.3 HAL
    * follows vendor/qcom/opensource/usb/vendor_product.mk
    * enable full usb function
    * partially ported sepolicy_vndr

--- device.mk
+++ device.mk
@@ -512,7 +512,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.3-service-qti
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
