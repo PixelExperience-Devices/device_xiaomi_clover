06e838c78c57142f03e80dc27177fd066f2c9398
Alcatraz323 <alcatraz32323@gmail.com>
2023-06-09 18:04:52 +0800

    clover: correct excluded-input-devices.xml location
    * we don't have vibrator on clover, so it's not neccessary
    * but why we need to keep it in a wrong place?

--- device.mk
+++ device.mk
@@ -572,7 +572,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
 PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/vendor/etc/excluded-input-devices.xml
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 PRODUCT_PACKAGES += \
