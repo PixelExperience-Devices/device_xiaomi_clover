5b875e1eac61debaebbef1bafbcc7c60f321c2e1
1582130940 <1582130940@qq.com>
2023-06-11 17:05:42 +0800

    clover: manifest: Add FCM to enforce vintf check
    * added wifidisplay, sensormanager, fingerprint, wifi, camera, power(up rev) interface
    * removed not existed vendor.qti.hardware.perf in manifest
    
    Co-authored-by: Alcatraz323 <alcatraz32323@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,6 +83,7 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 LOC_HIDL_VERSION := 4.0
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
