5530c16ddf9040abe252e0aa468fb8125acdbc23
Mathieu Chartier <mathieuc@google.com>
2023-07-17 12:10:49 +0800

    clover: Always preopt extracted APKs
    Change-Id: I2e4742a796b8142f14eaad5eba81eaf800d64914

--- device.mk
+++ device.mk
@@ -133,6 +133,7 @@ PRODUCT_PACKAGES += \
    DeviceSettings
 
 # Dexpreopt - Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
