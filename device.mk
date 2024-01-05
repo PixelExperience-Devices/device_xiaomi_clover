5692031437fba589093b47ef0fe0c20ab03a25f7
pix106 <sbordenave@gmail.com>
2024-01-05 16:22:24 +0800

    clover: build libprotobuf-cpp-lite-3.9.1-vendorcompat

--- device.mk
+++ device.mk
@@ -450,7 +450,8 @@ PRODUCT_COPY_FILES += \
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
-    libprotobuf-cpp-lite-vendorcompat
+    libprotobuf-cpp-lite-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
